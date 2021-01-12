27ba132b0047e4bfff65a6fa9873700164d1804e
Mahesh <maheshadapa.lr@gmail.com>
2021-01-12 15:48:04 +0530

    Login script first

---
+++ login/Action.c
@@ -0,0 +1,36 @@
+Action()
+{
+
+	web_url("webtours", 
+		"URL=http://127.0.0.1:1080/webtours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	/*launch*/
+
+	web_submit_data("login.pl", 
+		"Action=http://127.0.0.1:1080/cgi-bin/login.pl", 
+		"Method=POST", 
+		"TargetFrame=body", 
+		"RecContentType=text/html", 
+		"Referer=http://127.0.0.1:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=userSession", "Value=129822.112920211zzHHHctpQDDDDDDDtAAzzpDtQz", ENDITEM, 
+		"Name=username", "Value=jojo", ENDITEM, 
+		"Name=password", "Value=bean", ENDITEM, 
+		"Name=login.x", "Value=55", ENDITEM, 
+		"Name=login.y", "Value=6", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		LAST);
+
+	/*login*/
+
+	return 0;
+}
